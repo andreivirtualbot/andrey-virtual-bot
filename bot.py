2a6bde7520a5e1e9110db438073691c34fc078e8
andreivirtualbot <rastamanpatrik@gmail.com>
2025-04-07 02:26:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,6 @@ WEBHOOK_URL = os.environ.get("RENDER_EXTERNAL_URL") + TOKEN
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
-# Обработка команд /start и обычных сообщений
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message,
@@ -20,7 +19,6 @@ def send_welcome(message):
 def echo_all(message):
     bot.reply_to(message, "Я слушаю тебя...")
 
-# Flask маршрут для Telegram
 @app.route('/' + TOKEN, methods=['POST'])
 def webhook():
     json_str = request.get_data().decode('UTF-8')
@@ -28,12 +26,10 @@ def webhook():
     bot.process_new_updates([update])
     return 'ok', 200
 
-# Установка вебхука при запуске
 @app.before_first_request
 def set_webhook():
     bot.remove_webhook()
     bot.set_webhook(url=WEBHOOK_URL)
 
-# Запуск Flask
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
