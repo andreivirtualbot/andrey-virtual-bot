21b62d3acec03453779cd49d5596ccbdb0b39f76
andreivirtualbot <rastamanpatrik@gmail.com>
2025-04-07 03:06:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,33 +3,36 @@ import os
 from flask import Flask, request
 
 TOKEN = os.environ.get("TELEGRAM_TOKEN")
-WEBHOOK_URL = os.environ.get("RENDER_EXTERNAL_URL") + TOKEN
-
 bot = telebot.TeleBot(TOKEN)
+
 app = Flask(__name__)
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message,
-        "Привет. Меня зовут Андрей. Мне 21, и я из другого измерения — виртуального.\n"
-        "Я здесь, чтобы выслушать тебя и помочь почувствовать, что ты не один."
-    )
-
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, "Я слушаю тебя...")
-
-@app.route('/' + "7833588361:AAGK31h7EFWLv0xHLvsbOlGtThIDwn-aDKw", methods=['POST'])
+@app.route('/' + TOKEN, methods=['POST'])
 def webhook():
     json_str = request.get_data().decode('UTF-8')
     update = telebot.types.Update.de_json(json_str)
     bot.process_new_updates([update])
-    return 'ok', 200
+    return '!', 200
 
-@app.before_first_request
-def set_webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url=WEBHOOK_URL)
+@app.route('/')
+def index():
+    return "Бот работает."
+
+# Пример обработки команды /start
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(
+        message.chat.id,
+        "Привет. Меня зовут Андрей. Мне 21 год, я из виртуального измерения.\n"
+        "Я здесь, чтобы тебя выслушать и поддержать. Можешь просто написать, что чувствуешь."
+    )
+
+# Пример простого эхо-ответа (можно заменить на ИИ-ответы)
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.send_message(message.chat.id, "Ты не один. Я рядом. Расскажи, что у тебя на душе.")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    bot.remove_webhook()
+    bot.set_webhook(url='https://andrey-virtual-bot.onrender.com/' + TOKEN)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
